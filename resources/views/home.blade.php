e7d8e7ceadd201d66f978ac67da92a22568e2e9a
MikePio <michelepiopilla@gmail.com>
2023-06-01 17:41:54 +0200

    Created home page and printed "Hello World"

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body style="font-family: Arial, Helvetica, sans-serif; background-color: #1A202C; color: #A0AEC0;">
+
+    <h1 style="color: #EF3B2D;">Hello Word</h1>
+
+</body>
+</html>
